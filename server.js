2ad533412253e378186dece772278d0304c185a3
Suman Gulapalemmu <sgulapal@cisco.com>
2019-11-09 23:28:23 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load config vars
+dotenv.config({ path: './config/dev.env' });
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode & on port ${PORT}`
+  )
+);
